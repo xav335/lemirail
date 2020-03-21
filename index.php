0b63d5cb40ae950a6f68980766ecc7a0a85d4101
javier <javier@BigApple.home>
2020-03-21 19:44:24 +0100

    Celine

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>JOSE'S MOVIE</title>
+<title>Mme Saint QUENTIN</title>
 </head>
 <body>
-<h2>JOSE'S MOVIE</h3>
-<p><a href="downloader.php?doc=Jose.m4v">Download the movie</a></p>
+<h2>BlancaNieves de Pablo berger</h3>
+<p><a href="downloader.php?doc=blancanieves.mp4">Téléchager le Film</a></p>
 
-<p><a href="iphone.html">Link Iphone</a></p>
+<p><a href="iphone.html">Lien en streaming</a></p>
 
 </body>
 </html>
