d1f6b9779fc25104925e4b7ccd2cefa91cc6bd2d
Javier <jgonzalez@iconeo.frm>
2021-06-29 23:52:28 +0200

    add bloc culture

--- index.php
+++ index.php
@@ -26,15 +26,15 @@
 </head>
 
 <body>
-<div class="large-3 medium-3 small-12 columns" style="text-align: center;"> 
-  <img src="img/logoEDA.jpg" alt="" />  
-</div>
-<div class="large-6 medium-6 small-12 columns" style="text-align: center;"> 
-   </br></br> <h2>Page de téléchargement des spectacles de nos élèves.</h2>
-</div>
-<div class="large-3 medium-3 small-12 columns" style="text-align: center;"> 
-  </br></br><img src="img/AFA.png" alt="" />  
-</div>
+    <div class="large-3 medium-3 small-12 columns" style="text-align: center;"> 
+      <img src="img/logoEDA.jpg" alt="" />  
+    </div>
+    <div class="large-6 medium-6 small-12 columns" style="text-align: center;"> 
+       </br></br> <h2>Page de téléchargement des spectacles de nos élèves.</h2>
+    </div>
+    <div class="large-3 medium-3 small-12 columns" style="text-align: center;"> 
+      </br></br><img src="img/AFA.png" alt="" />  
+    </div>
 
   <div class="large-12 medium-12 small-12 columns" style="text-align: justify;">
     </br></br> 
@@ -51,36 +51,74 @@
   
   <div class="large-12 medium-12 small-12 columns" style="text-align: center;">
       <div class="callout secondary">
-         <h3>Pour faire un don :</h3><h4><a id="donner" href="https://www.helloasso.com/associations/association-francaise-de-l-atresie-de-l-sophage/collectes/spectacle-st-medard-en-jalles-maud-pasquet" class="button" target="_blank">FAIRE UN DON A NOTRE ASSOCIATION</a></h4>
+         <h3>Pour faire un don :</h3><h5><a id="donner" href="https://www.helloasso.com/associations/association-francaise-de-l-atresie-de-l-sophage/collectes/spectacle-st-medard-en-jalles-maud-pasquet" class="button" target="_blank">FAIRE UN DON A NOTRE ASSOCIATION</a></h4>
       </div>
   </div>
-  <div class="large-4 medium-4 small-12 columns" style="text-align: center;">
-     &nbsp;
+    <hr></hr>
+  <div class="large-12 medium-12 small-12 columns" style="text-align: center;">
+      <div class="large-4 medium-4 small-12 columns" style="text-align: center;">
+         &nbsp;
+      </div>
+      <div class="large-4 medium-4 small-12 columns" style="text-align: center;">
+        <form class="grid-x grid-padding-x" data-animation="top" id="formulaire" name="formulaire" method="post">
+    		<h3>Pour télécharger :</h3>
+    		<input type="text" name="nom" id="nom" placeholder="Indiquez vos nom et prénom" required  />
+    		<input type="email" name="email" id="email" placeholder="Indiquez votre e-mail" required />
+    	</form>
+      </div>
+       <div class="large-4 medium-4 small-12 columns" style="text-align: center;">
+        &nbsp;
+      </div>
   </div>
-  <div class="large-4 medium-4 small-12 columns" style="text-align: center;">
-            <form class="grid-x grid-padding-x" data-animation="top" id="formulaire" name="formulaire" method="post">
-					<h3>Pour télécharger :</h3>
-					<input type="text" name="nom" id="nom" placeholder="Indiquez vos nom et prénom" required  />
-					<input type="email" name="email" id="email" placeholder="Indiquez votre e-mail" required />
-			</form>
+<hr></hr>
+   <div class="large-12 medium-12 small-12 columns" style="text-align: center;">
+      <div class="callout secondary">
+         <h3>Spectacles des élèves</h3></div>
   </div>
-   <div class="large-4 medium-4 small-12 columns" style="text-align: center;">
-     &nbsp;
+  <div class="large-4 medium-4 small-12 columns" style="text-align: center;"> 
+      <div class="callout secondary">
+           <h5>LE SPECTACLE DES CM - 6ème </br> <a href="downloader.php?doc=PeterPanHD.mp4" target="_blank" id="telecharger" class="button" >TELECHARGER / VOIR</a></h5>
+      </div>
+  </div>
+  <div class="large-4 medium-4 small-12 columns" style="text-align: center;"> 
+      <div class="callout secondary">
+           <h5>LE SPECTACLE DES CE - 5ème </br> <a href="downloader.php?doc=PeterPanHD.mp4" target="_blank" id="telecharger2" class="button" >TELECHARGER / VOIR </a></h4>
+      </div>
+  </div>
+  <div class="large-4 medium-4 small-12 columns" style="text-align: center;"> 
+      <div class="callout secondary">
+           <h5>LE SPECTACLE DES  -  </br> <a href="downloader.php?doc=PeterPanHD.mp4" target="_blank" id="telecharger3" class="button" >TELECHARGER / VOIR </a></h4>
+      </div>
   </div>
   
-  <div class="large-12 medium-12 small-12 columns" style="text-align: center;"> 
+  <hr></hr>
+  <div class="large-12 medium-12 small-12 columns" style="text-align: center;">
       <div class="callout secondary">
-           <h4><a href="downloader.php?doc=PeterPanHD.mp4" target="_blank" id="telecharger" class="button" >TELECHARGER / VOIR LE SPECTACLE DES CM & 6ème (3,5Go)</a></h4>
+         <h3>Travaux du bloc culture des 5ème</h3></div>
+  </div>
+    
+  <div class="large-4 medium-4 small-12 columns" style="text-align: center;"> 
+      <div class="callout secondary">
+           <h5>LES INTERVIEWS DES CHEVALIERS </br> <a href="https://www.dropbox.com/sh/k5qqmcn4rivdi9a/AAA90HrxZF8EGInyiUfwfT1sa?dl=0" target="_blank" id="telecharger4" class="button" >TELECHARGER / VOIR </a></h4>
       </div>
   </div>
+  <div class="large-4 medium-4 small-12 columns" style="text-align: center;"> 
+      <div class="callout secondary">
+           <h5>LE defi astro PI </br> <a href="https://esero.fr/actualites/astropi-mission-zero-les-programmes-des-jeunes-a-bord-de-liss/" target="_blank" id="telecharger5" class="button" >félicitations de T.Pesquet</a></h4>
+      </div>
+  </div>
+  <div class="large-4 medium-4 small-12 columns" style="text-align: center;"> 
+      <div class="callout secondary">
+           <h5>LE 1er Eco-magasine du collège</br> <a href="downloader.php?doc=PeterPanHD.mp4" target="_blank" id="telecharger5" class="button" >consulter en ligne</a></h5>
+      </div>
  <!-- 
    <div class="large-12 medium-12 small-12 columns" style="text-align: center;"> 
       <div class="callout secondary">
-           <h4>ou :</h4><h4><a href="peter.html" id="streaming" target="_blank" class="button">Voir en streaming</a></h4>
+           <h5>ou :</h4><h5><a href="peter.html" id="streaming" target="_blank" class="button">Voir en streaming</a></h4>
       </div>
   </div>
    -->
-  
+   </br></br>  </br></br>  </br></br>  </br></br> 
 <?php include('inc/scripts.php'); ?>
 </body>
 <script type="text/javascript">
@@ -108,8 +146,56 @@
 		 //return false;
 	})
 
+	//-------#######------- cm6eme ----------#########-------//
+	$(document).on('click','#telecharger',function(e) {   //#####   Achanger
+
+	if (document.getElementById("nom").value.length == 0) {
+
+		alert("Veuillez renseigner votre nom svp.");
+	   
+	} else if (!document.getElementById("email").value.match(mailformat))  {
+
+		alert("Veuillez renseigner votre email svp.");
+
+	} else {	
 	
-	$(document).on('click','#telecharger',function(e) {
+		//document.location.href="https://www.dropbox.com/s/vjge97ycj49omjj/PeterPanHD.mp4?dl=0";
+		window.open('https://www.dropbox.com/s/vjge97ycj49omjj/PeterPanHD.mp4?dl=0', '_blank');  //#####   Achanger
+	  	e.preventDefault();
+	  	data = $("#formulaire").serializeArray();
+
+	 	 data.push({
+	   		name: 'action',
+	    	value: 'sendMail'
+	  	},
+		{
+	   		name: 'file',
+	    	value: 'cm6eme'   //#####   Achanger
+	  	})
+
+	  console.log(data);
+
+	    /* I put the above code for check data before send to ajax*/
+	    $.ajax({
+		        url: '/ajax/contact.php',
+		        type: 'post',
+		        data: data,
+		        success: function (data) {
+		            $("#resultat").html("<hr><h2 class='sous-titre'>Merci pour votre message</h2><hr>");
+		        	//$("#nom").val("");
+		           	//$("#email").val("");
+		        },
+		        error: function() {
+		        	 $("#resultat").html("<h3>Une erreur s'est produite !</h3>");
+		        }
+		   	});
+		}
+	return false;
+	})
+	//-------#######------- ######### ----------#########-------//
+	
+	//-------#######------- ce5eme ----------#########-------//
+	$(document).on('click','#telecharger2',function(e) {
 
 	if (document.getElementById("nom").value.length == 0) {
 
@@ -132,7 +218,7 @@
 	  	},
 		{
 	   		name: 'file',
-	    	value: 'cm6eme'
+	    	value: 'ce5eme'
 	  	})
 
 	  console.log(data);
@@ -144,8 +230,8 @@
 		        data: data,
 		        success: function (data) {
 		            $("#resultat").html("<hr><h2 class='sous-titre'>Merci pour votre message</h2><hr>");
-		        	$("#nom").val("");
-		           	$("#email").val("");
+		        	//$("#nom").val("");
+		           	//$("#email").val("");
 		        },
 		        error: function() {
 		        	 $("#resultat").html("<h3>Une erreur s'est produite !</h3>");
@@ -154,6 +240,6 @@
 		}
 	return false;
 	})
-
+	//-------#######------- ######### ----------#########-------//
 </script>
 </html>
